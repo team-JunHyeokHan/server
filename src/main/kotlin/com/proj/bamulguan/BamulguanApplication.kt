b90a0ea5e0f1e11092fe1445a0cd693b02e24b6b
김가영 <yeong.kmj@gmail.com>
2024-11-11 12:24:32 +0900

    feat: init

---
+++ src/main/kotlin/com/proj/bamulguan/BamulguanApplication.kt
@@ -0,0 +1,11 @@
+package com.proj.bamulguan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BamulguanApplication
+
+fun main(args: Array<String>) {
+    runApplication<BamulguanApplication>(*args)
+}
---
+++ src/test/kotlin/com/proj/bamulguan/BamulguanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.proj.bamulguan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BamulguanApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
